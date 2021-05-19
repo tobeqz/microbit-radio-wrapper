86c49da198a0e6a7d4fd9c2ec0cd12cbd974ebb7
Tobeqz <none>
2021-05-19 15:48:20 +0200

    test max string length

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+radio.setGroup(1)
+
+radio.sendString("123456789.123456789.123456789.123456789.123456789")
+radio.onRecievedString(str => {
+    console.log(str, str.length)
 })
