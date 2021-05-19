e66e2eda39723f830d9bdf1a797fd2a63b2c9263
tobeqz <83136264+tobeqz@users.noreply.github.com>
2021-05-19 16:14:59 +0200

    fix test

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 radio.setGroup(1)
 
 radio.sendString("123456789.123456789.123456789.123456789.123456789")
-radio.onRecievedString(str => {
-    console.log(str, str.length)
+radio.onReceivedString(str => {
+    console.log(str)
+    console.log(str.length)
 })
