2eb32dffe18045d7f6abc648b81b10f9f9765a2d
Tobeqz <none>
2021-05-19 17:58:47 +0200

    Added hexadecimal parser

--- main.ts
+++ main.ts
@@ -1,6 +1,66 @@
 radio.onReceivedNumber(num => {
     console.log(num)
 })
+
+const num_to_hex = "0123456789abcdef"
+
+const hex_to_num {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "a": 10,
+    "b": 11,
+    "c": 12,
+    "d": 13,
+    "e": 14,
+    "f": 15
+}
+/*
+ * Makecode heeft natuurlijk geen .toString(16)
+ * en ook geen parseInt(16)
+ */
+class Hexadecimal {
+    string: string,
+    num: number
+
+    constructor(num: string | number) {
+        if (typeof num == "string") {
+            this.string = num
+            let total = 0
+
+            for (let i = num.length-1; i >= 0; i--) {
+                total += hex_to_num[num[i]] * Math.pow(16, i)
+            }
+
+            self.num = total
+        } else if (typeof num == "number") {
+            this.num = num
+            this.string = ""
+
+            let rest = num
+
+            const remainders: number[] = []
+
+            while (rest > 0) {
+                const remainder = rest % 16
+                const rest = Math.floor(num / 16)
+                remainders.push(remainder)
+            }
+
+            for (let i = remainders.length-1; i >= 0; i--) {
+                this.string += num_to_hex[í]
+            }
+        }
+
+    }
+}
 /* 
  * Deze class is nodig omdat je maximaal 18 bytes
  * aan data kunt sturing via 1 sendString() call
@@ -32,7 +92,7 @@ class RadioWrapper {
         
         for (let i = 0; i < char_codes.length; i++) {
             const char_code = char_codes[i]
-            encoded_string += char_code.toString(16) // 16, hexadecimaal
+            encoded_string += new Hexadecimal(char_code).string
         }
         
         const string_parts = []
